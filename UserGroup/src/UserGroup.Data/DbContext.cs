07943849f41bec7904e1c01605ce5ddfc98482d9
Mark Michaelis <mark@IntelliTect.com>
2021-05-25 15:42:26 -0700

    Added dblogging to a file

--- UserGroup/src/UserGroup.Data/DbContext.cs
+++ UserGroup/src/UserGroup.Data/DbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,31 @@ namespace UserGroup.Data
 
         public DbSet<Event> Events => Set<Event>();
         public DbSet<Speaker> Speakers => Set<Speaker>();
+
+        private StreamWriter LogStream { get; } = new StreamWriter("dblog.txt", append: true);
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder is null)
+            {
+                throw new ArgumentNullException(nameof(optionsBuilder));
+            }
+
+            optionsBuilder.LogTo(LogStream.WriteLine);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            LogStream.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await LogStream.DisposeAsync();
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
