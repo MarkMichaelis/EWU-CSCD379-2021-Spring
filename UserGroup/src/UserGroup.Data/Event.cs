2aacf6276ec5a7eb5519dbf6bd8c99927d845945
Mark Michaelis <mark@IntelliTect.com>
2021-05-27 12:27:46 -0700

    Completed simple Add with DbContext

--- UserGroup/src/UserGroup.Data/Event.cs
+++ UserGroup/src/UserGroup.Data/Event.cs
@@ -5,9 +5,6 @@ namespace UserGroup.Data
 {
     public class Event
     {
-        public Event() { }
-        public Event(int id, string name) => (Id, Name) = (id, name);
-
         public int Id { get; set; }
         public string Title { get; set; } = "";
         public string? Description { get; set; } = "";
---
+++ UserGroup/src/UserGroup.Data/Migrations/20210527192148_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserGroup.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Location = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Speakers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Speakers", x => x.Id);
+                    table.UniqueConstraint("AK_Speakers_FirstName_LastName", x => new { x.FirstName, x.LastName });
+                    table.ForeignKey(
+                        name: "FK_Speakers_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Speakers_EventId",
+                table: "Speakers",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Speakers");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
